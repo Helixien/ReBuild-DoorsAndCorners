94a33ee8eda8bbc70aa0da8bf40d8e7f530334c4
Taranchuk <nfhfyxer@gmail.com>
2024-05-11 12:37:47 +0300

    damage dealt by entities to containment walls will be halved

---
+++ 1.5/Source/RB_DefOf.cs
@@ -0,0 +1,11 @@
+﻿using RimWorld;
+using Verse;
+
+namespace ReBuildDoorsAndCorners
+{
+    [DefOf]
+    public static class RB_DefOf
+    {
+        [MayRequireAnomaly] public static ThingDef RB_ContainmentWall;
+    }
+}
---
+++ 1.5/Source/ReBuildDoorsAndCornersMod.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace ReBuildDoorsAndCorners
+{
+    public class ReBuildDoorsAndCornersMod : Mod
+    {
+        public ReBuildDoorsAndCornersMod(ModContentPack pack) : base(pack)
+        {
+            new Harmony("ReBuildDoorsAndCornersMod").PatchAll();
+        }
+    }
+}
