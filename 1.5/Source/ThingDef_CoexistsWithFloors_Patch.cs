acf4fba5f9428cbb2b8ab50b9fa2c306b1030ac5
Taranchuk <nfhfyxer@gmail.com>
2024-09-18 20:27:46 +0300

    allow floors to be built under glass walls

---
+++ 1.5/Source/ThingDef_CoexistsWithFloors_Patch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace ReBuildDoorsAndCorners
+{
+    [HarmonyPatch(typeof(ThingDef), "CoexistsWithFloors", MethodType.Getter)]
+    public static class ThingDef_CoexistsWithFloors_Patch
+    {
+        public static void Postfix(ref bool __result, ThingDef __instance)
+        {
+            if (__result is false && __instance.HasComp<CompGlassWall>())
+            {
+                __result = true;
+            }
+        }
+    }
+}
